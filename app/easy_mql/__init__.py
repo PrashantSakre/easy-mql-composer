f6858f0134da5a8cb687373736dcfcf000c4633b
vivek-shrikhande <65864138+vivek-shrikhande@users.noreply.github.com>
2021-05-23 23:46:33 +0530

    feat: Add logging facility

---
+++ app/easy_mql/__init__.py
@@ -0,0 +1,34 @@
+import logging.handlers
+import time
+
+
+# FORMATTER
+class UTCFormatter(logging.Formatter):
+    default_msec_format = '%s.%03d'
+    converter = time.gmtime
+
+
+detailed = UTCFormatter(
+    fmt="[{levelname:8}] [{processName}] [{threadName}] [{asctime} UTC] [{lineno}:{name}] [{message}]",
+    style='{',
+)
+
+# HANDLERS
+# console
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+console_handler.setFormatter(detailed)
+# Rotating file
+LOG_FILE_NAME = 'app/.logs/easy-mql-webtool.log'
+LOG_FILE_SIZE = 2097152  # 2 MiB
+file_handler = logging.handlers.RotatingFileHandler(
+    LOG_FILE_NAME, maxBytes=LOG_FILE_SIZE, backupCount=10
+)
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(detailed)
+
+# logger
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+root.addHandler(console_handler)
+root.addHandler(file_handler)
