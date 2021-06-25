ec0a11d56dc04a8b96c19c98e776dabe287b28f2
vivek-shrikhande <65864138+vivek-shrikhande@users.noreply.github.com>
2021-06-25 23:39:09 +0530

    feat: Change editor1 buttons' styling

--- app/static/javascript/ajax.js
+++ app/static/javascript/ajax.js
@@ -1,4 +1,8 @@
-let selected_db, selected_collection;
+var isConnected = false;
+var selected_db = null;
+var selected_collection = null;
+var conn_button = document.getElementById('conn-button');
+var modal = document.getElementById("connectModal");
 
 // On click Result and Query buttons
 const btn_result = document.getElementById("button-result");
@@ -49,6 +53,38 @@ function convert() {
     httpRequest.send();
 }
 
+conn_button.addEventListener('click', () => {
+    if (isConnected) {
+        // DISCONNECT
+        isConnected = false;
+
+        // set button state to connect
+        conn_button.innerText = 'Connect';
+        conn_button.classList.remove('disconn-button');
+
+        // clear the db and coll dropdown list empty
+        let emptyMenuItem = '<button class="dropdown-item">Empty</button>';
+        document.getElementById('db-dropdown-menu').innerHTML = emptyMenuItem;
+        document.getElementById('db-dropdown-button').innerText = 'Db';
+        document.getElementById('coll-dropdown-menu').innerHTML = emptyMenuItem;
+        document.getElementById('coll-dropdown-button').innerText = 'Coll';
+
+    } else {
+        // CONNECT
+
+        // When the user clicks the button, open the modal
+        modal.style.display = "block";
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+    }
+})
+
 // connection to the mongo
 const connect_mongo = document.getElementById("connect_mongo");
 
@@ -60,19 +96,21 @@ connect_mongo.addEventListener('click', () => {
     mongoUrl = document.getElementById('mongo-url').value;
     mongoHttpRequest.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200 ) {
-            // change connect button to disabled vice-versa with disconnect button
-            $(".connect").addClass("disabled");
-            $(".disconnect").removeClass("disabled");
-            // change the color of db-icon to lime
-            document.getElementById('button-connect').style.cssText = 'color: lime;';
+            isConnected = true;
+
+            // change the state of button to disconnected
+            conn_button.innerText = 'Disconnect';
+            conn_button.classList.add('disconn-button');
+
             // closes the pop up window after the connection to db
             modal.style.display = "none";
             array = JSON.parse(this.responseText)
+
             var newHTML = [];
             for (var i = 0; i < array.length; i++) {
-                newHTML.push('<button class="btn-sm dropdown-item" onClick="select_dbs()">' + array[i] + '</button>');
+                newHTML.push('<button class="dropdown-item" onClick="select_dbs(event)">' + array[i] + '</button>');
             }
-            $(".dropdown-db").html(newHTML.join(''));
+            document.getElementById('db-dropdown-menu').innerHTML = newHTML.join('');
             $('.modal-content button').text('Connect');
         } else if ( this.status == 500 ) {
             $('.modal-error').html('<p>Internal server error check your mongo connection</p>');
@@ -85,27 +123,27 @@ connect_mongo.addEventListener('click', () => {
 });
 
 
-function select_dbs() {
+function select_dbs(event) {
     selected_db = event.target.innerText;
-    document.getElementById('dropdownSelectedDbButton').innerText = selected_db;
+    document.getElementById('db-dropdown-button').innerText = selected_db;
     collectionHttpRequest = new XMLHttpRequest();
     collectionHttpRequest.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200 ) {
             collectionsArray = JSON.parse(this.responseText);
             var newHTMLs = [];
             for (var i = 0; i < collectionsArray.length; i++) {
-                newHTMLs.push('<button class="btn-sm dropdown-item" onClick="select_collection()">' + collectionsArray[i] + '</button>');
+                newHTMLs.push('<button class="dropdown-item" onClick="select_collection(event)">' + collectionsArray[i] + '</button>');
             }
-            $(".dropdown-collection").html(newHTMLs.join(''));
+            document.getElementById('coll-dropdown-menu').innerHTML = newHTMLs.join('');
         }
     }
     collectionHttpRequest.open('GET', '/dbs/'+selected_db+'/collections');
     collectionHttpRequest.send();
 }
 
-function select_collection() {
+function select_collection(event) {
     selected_collection = event.target.innerText;
-    document.getElementById('dropdownSelectedCollectionButton').innerText = selected_collection;
+    document.getElementById('coll-dropdown-button').innerText = selected_collection;
     documentsHttpRequest = new XMLHttpRequest();
     documentsHttpRequest.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200 ) {
@@ -143,3 +181,62 @@ function disconnect_button() {
     dbDisconnectHttpRequest.open('POST', '/disconnect');
     dbDisconnectHttpRequest.send();
 }
+
+let run_button = document.getElementById("run-button");
+
+run_button.addEventListener('click', () => {
+    if (!isConnected) {
+        alert('Please connect to server');
+        return;
+    } else if (!selected_db) {
+        alert('Please select a database');
+        return;
+    } else if (!selected_collection) {
+        alert('Please select a collection');
+        return;
+    }
+    
+    // Ajax http request
+    httpRequest = new XMLHttpRequest();
+    editor1_body = editor1.getValue();
+    httpRequest.onreadystatechange = function () {
+        if (this.readyState == 4 ) {
+            setTimeout(5000);
+            
+            if (this.status == 200) {
+                editor2.session.setMode("ace/mode/json");
+                let response = JSON.parse(this.responseText);
+                // add event listener for result button
+                btn_result.addEventListener('click', () => {
+                    editor2.setValue(JSON.stringify(response.result, null, 4));
+                    editor2.clearSelection();
+                    btn_result.style.cssText = 'color: #f7f7f7; background-color: #393e46;';
+                    btn_query.style.cssText = 'color: #393e46; background-color: #929aab;';
+                });
+                // add event listener for query button
+                btn_query.addEventListener('click', () => {
+                    editor2.setValue(JSON.stringify(response.query, null, 4));
+                    editor2.clearSelection();
+                    btn_query.style.cssText = 'color: #f7f7f7; background-color: #393e46;';
+                    btn_result.style.cssText = 'color: #393e46; background-color: #929aab;';
+                });
+                // set response from server to editor2
+                editor2.setValue(JSON.stringify(response.result, null, 4));
+                editor2.clearSelection();
+                isIndented = true;
+                btn_result.style.cssText = 'color: #f7f7f7; background-color: #393e46;';
+                btn_query.style.cssText = 'color: #393e46; background-color: #929aab;';
+                indent_editor2_btn.style.cssText = 'color: #f7f7f7; background-color: #393e46;';
+            } else {
+                // if query is wrong change editor to python and set the value
+                btn_result.style.cssText = 'color: #393e46; background-color: #929aab;';
+                btn_query.style.cssText = 'color: #393e46; background-color: #929aab;';
+                editor2.session.setMode("ace/mode/python");
+                editor2.setValue(this.responseText);
+                editor2.clearSelection();
+            }
+        }
+    }
+    httpRequest.open('GET', `/dbs/${selected_db}/collections/${selected_collection}/docs?query=${encodeURIComponent(editor1_body)}`);
+    httpRequest.send();
+});
