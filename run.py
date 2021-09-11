bae0752b59ab06c205625d6d340feb2d277019e9
vivek-shrikhande <65864138+vivek-shrikhande@users.noreply.github.com>
2021-09-11 23:01:53 +0530

    chore: Dockerize

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
+import os
+
 from app.easy_mql.flask_mql import app
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host=os.getenv('HOST', '0.0.0.0'), port=os.getenv('PORT', '5000'))
